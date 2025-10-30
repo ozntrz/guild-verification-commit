c7bab49d9f91e90ae5a716b9ffce8371472d20e9
ozntrz <133866062+ozntrz@users.noreply.github.com>
2025-10-31 01:00:15 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-10-30T22:00:12.061Z
-Commit: Add multi-language support
+Timestamp: 2025-10-30T22:00:14.716Z
+Commit: Patch security vulnerability in authentication
