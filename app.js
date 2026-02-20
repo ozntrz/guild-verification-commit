b1152418bfc2b15a33b458185f7a27279a95f4fe
ozntrz <133866062+ozntrz@users.noreply.github.com>
2026-02-21 00:12:44 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-20T21:12:26.306Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-02-20T21:12:43.040Z
+Commit: Fix broken links in documentation
