17b5a73a7bda4703cd925bc0c69f1b967616e5c2
ozntrz <133866062+ozntrz@users.noreply.github.com>
2026-02-21 00:14:30 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-20T21:14:16.688Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-02-20T21:14:29.267Z
+Commit: Resolve null pointer exception in user service
