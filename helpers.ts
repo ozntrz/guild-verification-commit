09ad321eec4eb497568597a220862145cb748262
ozntrz <133866062+ozntrz@users.noreply.github.com>
2026-02-21 00:13:55 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-20T21:13:39.990Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-02-20T21:13:54.945Z
+Commit: Patch security vulnerability in authentication
