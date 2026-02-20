c38ebe3039726bc6732508c9a46192b810cfcb1d
ozntrz <133866062+ozntrz@users.noreply.github.com>
2026-02-21 00:12:40 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-20T21:12:21.742Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-02-20T21:12:39.358Z
+Commit: Fix broken links in documentation
