551627c27aaf36e1a8e1dae25ded385299e4da86
ozntrz <133866062+ozntrz@users.noreply.github.com>
2026-02-21 00:12:54 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-20T21:12:50.548Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-02-20T21:12:53.478Z
+Commit: Fix broken links in documentation
