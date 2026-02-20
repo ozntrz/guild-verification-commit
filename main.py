0ec74786bdec043e1da93a2c6d83dda976499785
ozntrz <133866062+ozntrz@users.noreply.github.com>
2026-02-21 00:12:51 +0300

    Fix CSS styling bug on mobile devices

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-20T21:12:39.358Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-02-20T21:12:50.548Z
+Commit: Fix CSS styling bug on mobile devices
