fa77a0a80764056631360f59b8320124ac3bbe8e
ozntrz <133866062+ozntrz@users.noreply.github.com>
2026-02-21 00:14:21 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-20T21:14:18.565Z
-Commit: Add export functionality for reports
+Timestamp: 2026-02-20T21:14:20.031Z
+Commit: Fix broken links in documentation
