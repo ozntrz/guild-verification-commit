3b209bc91540cfd27c28ee03a5cf08eb765f74cd
ozntrz <133866062+ozntrz@users.noreply.github.com>
2026-02-21 00:13:09 +0300

    Resolve timezone issues in date handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-20T21:13:05.184Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-02-20T21:13:08.298Z
+Commit: Resolve timezone issues in date handling
