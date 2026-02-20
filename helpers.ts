1df5b99588e46b310ec74fbafbf7bb20eac8a3b3
ozntrz <133866062+ozntrz@users.noreply.github.com>
2026-02-21 00:13:41 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-20T21:13:17.668Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-02-20T21:13:39.990Z
+Commit: Fix broken links in documentation
