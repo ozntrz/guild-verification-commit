ccdad5a19e9292ce0bdff806c6a81640ee781993
ozntrz <133866062+ozntrz@users.noreply.github.com>
2026-02-21 00:13:06 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-20T21:13:00.346Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-02-20T21:13:05.184Z
+Commit: Fix broken links in documentation
