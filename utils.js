e6c6c8f2ff9bcaf323440e613ce34e46f5a33360
ozntrz <133866062+ozntrz@users.noreply.github.com>
2026-02-21 00:13:58 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-20T21:13:51.671Z
-Commit: Add export functionality for reports
+Timestamp: 2026-02-20T21:13:56.991Z
+Commit: Fix memory leak in event handlers
