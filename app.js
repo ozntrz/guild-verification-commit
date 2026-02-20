fcb0d0b93a8d860e0380fde532fc361fc801d46b
ozntrz <133866062+ozntrz@users.noreply.github.com>
2026-02-21 00:13:12 +0300

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-20T21:13:03.699Z
-Commit: Add dark mode support
+Timestamp: 2026-02-20T21:13:11.470Z
+Commit: Resolve null pointer exception in user service
